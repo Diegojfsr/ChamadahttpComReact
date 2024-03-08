8d77e51cac2179c8e39bc6ecd08c782b30a92301
Diego Jefferson <diegojfsr@gmail.com>
2024-03-08 12:48:11 -0300

    Variaveis

---
+++ Exercicios Resolvidos/Variaveis.js
@@ -0,0 +1,11 @@
+/*
+Escreva um programa que declare 2 variaveis: Nome e Idade.
+Imprima a frase: " Olá meu nome é: (nome) e eu tenho (Idade) anos."
+*/
+
+let nome = "Diego";
+let idade = 29;
+
+console.log("Olá meu nome é " + nome + " e eu tenho " + idade + " anos.");
+
+
