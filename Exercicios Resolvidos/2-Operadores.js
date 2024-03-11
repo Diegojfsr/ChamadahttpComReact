62e3f2c3885e74351d062377f80c29e6e26cf33d
Diego Jefferson <diegojfsr@gmail.com>
2024-03-11 13:41:32 -0300

    Operadores

---
+++ Exercicios Resolvidos/2-Operadores.js
@@ -0,0 +1,16 @@
+
+/*
+Crie um programa que receba dois números.
+Exiba o resultado da soma, subtração, multiplicação e divisão.
+*/
+
+let num1 = 2;
+let num2 = 5;
+let soma = num1 + num2;
+let subtracao = num1 - num2;
+let mutiltiplicacao = num1 + num2;
+let divisao = num1 / num2;
+console.log("A soma é: " + soma + " a subtração é: " + subtracao + " a multiplicação é: " + mutiltiplicacao + " a divisão é: " + divisao);
+
+
+
