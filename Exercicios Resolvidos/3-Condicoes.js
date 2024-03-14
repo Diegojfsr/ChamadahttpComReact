bbd8529a1d5410240c59a91d048d684e4b110926
Diego Jefferson <diegojfsr@gmail.com>
2024-03-13 22:24:18 -0300

    Condicoes

---
+++ Exercicios Resolvidos/3-Condicoes.js
@@ -0,0 +1,18 @@
+
+/*
+Escreva um programa que receba o nome de um usuáro e sua idade.
+Imprima no console se ele é maior ou menor de idade.
+*/
+
+let nomeUser = "Diego";
+let idadeUser = 29;
+
+if (idadeUser >= 18){
+    console.log(nomeUser + " é Maior de idade.");
+}else{
+    console.log(nomeUser + " é Menor de idade.");
+}
+
+
+
+
