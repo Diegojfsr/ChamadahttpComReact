a36085d135324b9a5f059d1b47d0d12a15f79331
Diego Jefferson <65885167+Diegojfsr@users.noreply.github.com>
2024-04-18 16:22:28 -0300

    Update Exemplo#2.js

--- Exercicicos/JavaScript Iniciante/Exemplo#2.js
+++ Exercicicos/JavaScript Iniciante/Exemplo#2.js
@@ -14,4 +14,13 @@ console.log(`A subtração é: ${num1 - num2}`);
 console.log(`A multiplicação é: ${num1 * num2}`);
 console.log(`A divisão é: ${num1 / num2}`);
 
+let num3 = prompt("Digite um número:");
+let num4 = prompt("Digite outro número:");
+num3 = Number(num3);
+num4 = Number(num4);
+console.log(`A soma é: ${num3 + num4}`);
+console.log(`A subtração é: ${num3 - num4}`);
+console.log(`A multiplicação é: ${num3 * num4}`);
+console.log(`A divisão é: ${num3 / num4}`);
+
 
