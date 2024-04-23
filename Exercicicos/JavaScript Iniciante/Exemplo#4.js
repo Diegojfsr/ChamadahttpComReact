b6f459ef53529bcaeb4301d329bb498762f9713d
Diego Jefferson <65885167+Diegojfsr@users.noreply.github.com>
2024-04-23 16:21:39 -0300

    Update Exemplo#4.js

--- Exercicicos/JavaScript Iniciante/Exemplo#4.js
+++ Exercicicos/JavaScript Iniciante/Exemplo#4.js
@@ -9,6 +9,11 @@ for (let i = 1; i <= 100; i++) {
     console.log(i);
 }
 
-for (let i = 1; i <= 10; i++) {
-    console.log(i);
+for (let j = 1; i <= 10; i++) {
+    console.log(j);
+}
+
+
+for (let g = 1; i <= 10; i++) {
+    console.log(g);
 }
